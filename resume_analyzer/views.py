74a19dba722b5dd5344c9e5244c3cfb5d251b4ec
myrsaliev21 <myrsalievdastan211104@gmail.com>
2024-10-18 19:31:45 +0600

    dastan_diploma_project

--- resume_analyzer/views.py
+++ resume_analyzer/views.py
@@ -1,5 +1,6 @@
 import requests
 from requests import Response
+from rest_framework.exceptions import NotAuthenticated
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -14,34 +15,42 @@ from rest_framework import permissions, generics
 class ResumeCreateAPIView(CreateAPIView):
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+
+        if self.request.user.is_anonymous:
+            raise NotAuthenticated("User must be authenticated to upload a resume.")
+
+        resume = serializer.save(user=self.request.user)
+
+        self.analyze_resume(resume)
 
     def analyze_resume(self, resume):
-        print("analyze_resume")
+        print("analyze_resume started")
         headers = {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"  # Замените на ваш токен
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"
         }
         url = "https://api.edenai.run/v2/ocr/resume_parser"
         json_payload = {
             "providers": "openai",
         }
 
-        with open(resume.file.path, "rb") as f:
-            files = {"file": f}
-            response = requests.post(url, data=json_payload, headers=headers, files=files)
+        try:
+            with open(resume.file.path, "rb") as f:
+                files = {"file": f}
+                response = requests.post(url, data=json_payload, headers=headers, files=files)
 
-        if response.status_code == 200:
-            result = response.json()
-            # Обработайте результат и сохраните его в поле description
-            resume.description = result  # Или выберите нужные поля из result
-            resume.analyzed = True
-            resume.save()
-        else:
-            # Обработайте ошибку, если необходимо
-            print(f"Error analyzing resume: {response.text}")
+            if response.status_code == 200:
+                result = response.json()
+                resume.description = result
+                resume.analyzed = True
+                resume.save()
+                print("Resume analyzed and saved successfully.")
+            else:
+                print(f"Error analyzing resume: {response.status_code} - {response.text}")
+        except Exception as e:
+            print(f"Exception while analyzing resume: {e}")
 
 
 class ResumeList(generics.ListAPIView):
