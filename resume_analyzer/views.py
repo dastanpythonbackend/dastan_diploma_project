28986c83d96ff914f38a2f84d789d5938fa48c64
myrsaliev21 <myrsalievdastan211104@gmail.com>
2024-10-23 20:02:21 +0600

    dastan_diploma_project

--- resume_analyzer/views.py
+++ resume_analyzer/views.py
@@ -10,6 +10,7 @@ from .models import Resume, ResumeAnalysis
 from .serializers import ResumeSerializer, ResumeAnalysisSerializer
 from rest_framework import permissions, generics
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
@@ -106,9 +107,92 @@ class ResumeAnalyzer:
 
         return {"status": "успех", "message": "реюме обработанно"}
 
+    def ai_content_detection(self):
+        headers = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"}
+
+        url = "https://api.edenai.run/v2/text/ai_detection"
+        payload = {
+            "providers": "originalityai",
+            "text": "Привет! У меня всё хорошо, спасибо. А как у тебя?",
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        result = json.loads(response.text)
+        return result
+
+    def emotion_detection(self):
+        headers = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"}
+
+        url = "https://api.edenai.run/v2/text/emotion_detection"
+        payload = {
+            "providers": "nlpcloud,vernai",
+            "text": "I am angry!",
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        result = json.loads(response.text)
+        return result["nlpcloud"]["items"]
+
+    def pii_and_anonymization(self):
+        headers = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"}
+
+        url = "https://api.edenai.run/v2/text/anonymization"
+        payload = {
+            "providers": "emvista",
+            "language": "en",
+            "text": "My name is Jeremy and this is a test",
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        result = json.loads(response.text)
+        return result['emvista']['result']
+
+    def question_answer(self):
+        headers = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"}
+
+        url = "https://api.edenai.run/v2/image/question_answer"
+        json_payload = {
+            "providers": "alephalpha",
+            "file_url": "🔗 URL of your image",
+            "question": "What are the logos on the image ?",
+        }
+
+        response = requests.post(url, json=json_payload, headers=headers)
+
+        result = json.loads(response.text)
+        return result['alephalpha']['answers']
+
+    def sentiment_analysis(self):
+        headers = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTRiMDQ0NTktZjQ1MS00OTY4LTlhM2UtYjhiNmI4ZTJhMDcwIiwidHlwZSI6ImFwaV90b2tlbiJ9.ukZ22dwwefVnF0zisTPYjCadMSSv7tGc983GrVTNWyw"}
+
+        url = "https://api.edenai.run/v2/text/sentiment_analysis"
+        payload = {
+            "providers": "google,amazon",
+            "language": "en",
+            "text": "this is a test",
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        result = json.loads(response.text)
+        return result['google']['items']
+
 
 def analyze_resume_view(request, resume_id):
     analyzer = ResumeAnalyzer(resume_id)
     result = analyzer.extract_and_save_analysis()
+    ai_content_detection = analyzer.ai_content_detection()
+    emotion_detection = analyzer.emotion_detection()
+    pii_and_anonymization = analyzer.pii_and_anonymization()
+    question_answer = analyzer.question_answer()
+    sentiment_analysis = analyzer.sentiment_analysis()
 
-    return JsonResponse(result)
+    return JsonResponse(result, ai_content_detection, emotion_detection, pii_and_anonymization,  question_answer, sentiment_analysis)
